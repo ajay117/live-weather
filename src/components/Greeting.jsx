a2710d80ceb73f1e049c8eb93a191fef31d133df
Ajay KC <meajay64@gmail.com>
2023-09-23 16:07:42 +0900

    Create Greeting Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,23 @@
+function Greetings({ timestamp }) {
+  let hours = new Date(timestamp * 1000).getHours();
+//   let minutes = new Date(timestamp * 1000).getMinutes();
+
+//   let time = `${hours}:${minutes}`;
+
+  let greeting;
+  if (hours >= 5 && hours < 12) {
+    greeting = "Good Morning";
+  } else if (hours >= 12 && hours < 18) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+
+  return (
+    <div>
+      <p>{greeting}</p>
+    </div>
+  );
+}
+
+export default Greetings;
