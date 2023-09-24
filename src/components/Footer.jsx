02356bf59880a58aa1373e8d444a778d1f159d22
Ajay KC <meajay64@gmail.com>
2023-09-24 23:19:18 +0900

    Add new component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div className="tracking-wide font-extralight text-center bg-gray-900 text-slate-200 absolute w-full bottom-0">
+      Copyright &copy;{" "}
+      <a
+        className="text-blue-600 border-b-2 border-blue-600"
+        href="https://ajaykc.netlify.app/"
+      >
+        Ajay KC
+      </a>
+      . All Rights Reserved.
+    </div>
+  );
+}
